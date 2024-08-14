f3f5cb10c546ef30bf24a1e5d6981785fdf1054c
MyeongSeop Shin <83411963+shinmyeongsub@users.noreply.github.com>
2024-08-14 09:43:11 +0900

    feat: Entity Class 개발
    Entity : Address, Delivery, Member, Order, OrderItem, Item(Abstract) [Book, Album, Movie]
    
    Value Type : Address
    
    Enum : OrderStatus, DeliveryStatus

---
+++ src/main/java/jpabook/jpashop/domain/Item/Book.java
@@ -0,0 +1,14 @@
+package jpabook.jpashop.domain.Item;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@DiscriminatorValue("B")
+@Getter @Setter
+public class Book extends Item {
+    private String author;
+    private int isbn;
+}
