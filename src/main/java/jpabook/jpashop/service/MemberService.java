cc16b63ec3edeb7398066d32685ee921d90863a7
MyeongSeop Shin <83411963+shinmyeongsub@users.noreply.github.com>
2024-08-19 18:18:19 +0900

    feat: API 개발 기본
    API 개발 고급 - 컬렉션 조회 최적화

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -39,12 +39,12 @@ public class MemberService {
 
     // 회원 한 건 조회
     public Member findOne(Long memberId) {
-        return memberRepository.findOne(memberId);
+        return memberRepository.findById(memberId).get();
     }
 
     @Transactional
     public void update(Long id, String name) {
-        Member member = memberRepository.findOne(id);
+        Member member = memberRepository.findById(id).get();
         member.setName(name);
     }
 }
